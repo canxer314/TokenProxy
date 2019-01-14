07793d8d636a5e9a61f84ffe2588ec6fe226f415
canxer314 <canxer314@live.com>
2019-01-14 11:47:26 +0800

    add redis configuration

---
+++ src/main/java/com/ecidi/cim/tokenproxy/config/RedisConfig.java
@@ -0,0 +1,77 @@
+package com.ecidi.cim.tokenproxy.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.*;
+
+import java.time.Duration;
+/**
+ * @class_name:  RedisConfig
+ * @package:     com.ecidi.cim.tokenproxy.config
+ * @author:      Canxer Huang
+ * @department:  Ecidi
+ * @date:        2019-1-14 10:33
+ * @description: Redis缓存配置
+ *
+ */
+
+@Slf4j
+@Configuration
+@EnableCaching
+@ConfigurationProperties(prefix = "spring.cache.redis")
+public class RedisConfig {
+    private Duration timeToLive = Duration.ZERO;
+    public void setTimeToLive(Duration timeToLive) {
+        this.timeToLive = timeToLive;
+    }
+
+    @Bean
+    public RedisCacheManager cacheManager(RedisConnectionFactory connectionFactory) {
+        RedisCacheConfiguration config = RedisCacheConfiguration.defaultCacheConfig()
+                .entryTtl(this.timeToLive)
+                .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(keySerializer()))
+                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(valueSerializer()))
+                .disableCachingNullValues();
+
+        RedisCacheManager redisCacheManager = RedisCacheManager.builder(connectionFactory)
+                .cacheDefaults(config)
+                .transactionAware()
+                .build();
+
+        log.info("自定义RedisCacheManager加载完成");
+        return redisCacheManager;
+    }
+
+    /**
+     * 配置自定义redisTemplate
+     * @return
+     */
+    @Bean(name = "redisTemplate")
+    public RedisTemplate<String,Object> redisTemplate(RedisConnectionFactory redisConnectionFactory){
+        RedisTemplate<String,Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(redisConnectionFactory);
+
+        redisTemplate.setKeySerializer(keySerializer());
+        redisTemplate.setHashKeySerializer(keySerializer());
+        redisTemplate.setValueSerializer(valueSerializer());
+        redisTemplate.setHashValueSerializer(valueSerializer());
+
+        log.info("自定义RedisTemplate加载完成");
+        return redisTemplate;
+    }
+
+    private RedisSerializer<String> keySerializer() {
+        return new StringRedisSerializer();
+    }
+
+    private RedisSerializer<Object> valueSerializer() {
+        return new GenericJackson2JsonRedisSerializer();
+    }
+}
